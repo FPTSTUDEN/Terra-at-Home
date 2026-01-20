8e130ef819733acba93ab7235a77711982a74ac3
DLog <doandinhanh307@gmail.com>
2026-01-20 10:58:34 +0700

    safe git pull script

--- pull.sh
+++ pull.sh
@@ -1,2 +1,14 @@
+git update-index --skip-worktree ./config/tsdproxy.yaml
+# to undo the skip-worktree flag, use:
+# git update-index --no-skip-worktree path/to/config.yml
+
 git fetch origin
-git reset --hard origin/main
+# prompt for user confirmation before pulling
+read -p "Are you sure you want to resolve above conflicts and pull the latest changes? (y/n): " confirmation
+if [[ $confirmation == "y" || $confirmation == "Y" ]]; then
+    #git discard changes
+    git reset --hard HEAD # discard local changes, gitignore not affected due to skip-worktree
+    git pull origin main
+else
+    echo "Pull operation cancelled."
+fi
